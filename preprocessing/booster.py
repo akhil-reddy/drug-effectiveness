2cce0f398a80389859cd7ea0084f0af387bb260f
Akhil K <blitzkrieg.akhil@gmail.com>
2024-03-11 22:01:58 -0500

    Preprocessing Implementation for Booster

--- preprocessing/booster.py
+++ preprocessing/booster.py
@@ -2,19 +2,25 @@ from tdc.multi_pred import Catalyst
 from tdc.multi_pred import DrugSyn
 from tdc.utils import get_label_map
 from indigo import *
+import pandas as pd
 
 indigo = Indigo()
-mol1 = indigo.loadMolecule("CN2C(=O)N(C)C(=O)C1=C2N=CN1C")
-mol2 = indigo.loadMolecule("CN1C=NC2=C1C(=O)N(C)C(=O)N2C")
-mol1.aromatize()
-mol2.aromatize()
-print(mol1.canonicalSmiles())
-print(mol2.canonicalSmiles())
-assert mol1.canonicalSmiles() == mol2.canonicalSmiles()
 
-data = Catalyst(name = 'USPTO_Catalyst')
-data2 = DrugSyn(name = 'DrugComb')
-data3 = DrugSyn(name = 'OncoPolyPharmacology')
-
+catalyst = Catalyst(name = 'USPTO_Catalyst')
 label_map = get_label_map(name = 'USPTO_Catalyst', task = 'Catalyst')
-print(label_map)
+drug_smiles = []
+catalyst_smiles = []
+
+for reactant, cat in zip(catalyst.entity1, catalyst.y):
+    drug = indigo.loadMolecule(reactant)
+    drug.aromatize()
+    drug_smiles.append(drug.canonicalSmiles())
+
+    catSmile = indigo.loadMolecule(label_map[cat])
+    catSmile.aromatize()
+    catalyst_smiles.append(catSmile.canonicalSmiles())
+
+catalyst_data = pd.DataFrame([drug_smiles, catalyst_smiles], columns=["smile", "catalyst"])
+drug_comb = DrugSyn(name = 'DrugComb')
+onco_poly = DrugSyn(name = 'OncoPolyPharmacology')
+
